43bbe5377741d57ddae8b75774f47faa6a84128d
Arimonte <arimonte2002@gmail.com>
2022-12-01 12:43:53 -0800

    novelty

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 
-for letter in range(97, 123):
-    if chr(letter) != 'q' and chr(letter) != 'e':
-        print("{}".format(chr(letter)), end="")
+[print("{}".format(chr(x)), end="")
+    for x in range(97, 123) if x != ord("q") and x != ord("e")]
