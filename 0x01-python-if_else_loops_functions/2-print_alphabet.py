8299d513ce4fd3033230b62b1da9abd59789152b
Arimonte <arimonte2002@gmail.com>
2022-12-04 12:37:22 -0800

    elevate

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
+"""Print the alphabet in lowercase, not followed by a new line."""
 
-[print("{}".format(chr(x)), end="") for x in range(97, 123)]
+for letter in range(97, 123):
+    print("{}".format(chr(letter)), end="")
