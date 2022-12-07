699663d4700f56dac9bae15dcdef6635365f72e3
Arimonte <arimonte2002@gmail.com>
2022-12-07 13:53:46 -0800

    piechart

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 #include <Python.h>
 
 /**
@@ -8,20 +7,16 @@
  * @p: Python Object
  * Return: no return
  */
-
 void print_python_bytes(PyObject *p)
-
 {
 	char *string;
 	long int size, i, limit;
 
 	printf("[.] bytes object info\n");
-
 	if (!PyBytes_Check(p))
 	{
 		printf("  [ERROR] Invalid Bytes Object\n");
 		return;
-
 	}
 
 	size = ((PyVarObject *)(p))->ob_size;
@@ -41,7 +36,7 @@ void print_python_bytes(PyObject *p)
 		if (string[i] >= 0)
 			printf(" %02x", string[i]);
 		else
-			printf(" %02x", 256 + string[i])
+			printf(" %02x", 256 + string[i]);
 
 	printf("\n");
 }
@@ -52,9 +47,7 @@ void print_python_bytes(PyObject *p)
  * @p: Python Object
  * Return: no return
  */
-
 void print_python_list(PyObject *p)
-
 {
 	long int size, i;
 	PyListObject *list;
@@ -68,12 +61,10 @@ void print_python_list(PyObject *p)
 	printf("[*] Allocated = %ld\n", list->allocated);
 
 	for (i = 0; i < size; i++)
-
 	{
 		obj = ((PyListObject *)p)->ob_item[i];
 		printf("Element %ld: %s\n", i, ((obj)->ob_type)->tp_name);
 		if (PyBytes_Check(obj))
 			print_python_bytes(obj);
-
 	}
 }
