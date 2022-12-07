08eb62744867dd9dfb40b59393f8ce420b94fb01
Arimonte <arimonte2002@gmail.com>
2022-12-07 14:01:18 -0800

    soup

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    ifa_dictionary. get(key)isnotNone:
+    if a_dictionary.get(key) is not None:
         del a_dictionary[key]
     return (a_dictionary)
